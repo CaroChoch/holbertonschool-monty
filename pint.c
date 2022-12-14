116df18365f0fe88648072643819e3ce79ed382a
CaroChoch <5692@holbertonstudents.com>
2022-12-13 17:31:46 -0800

    betty coding style

--- pint.c
+++ pint.c
@@ -9,7 +9,10 @@
 void pint(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
-		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number), exit(EXIT_FAILURE);
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
 	printf("%d\n", (*stack)->n);
 }
