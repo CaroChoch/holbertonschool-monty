a57698197eb8937be3d72b6a87e7cc9faeee70d2
Holberton Caro <holbertoncaro@macbook-pro-de-chochoy.home>
2022-12-14 14:32:22 +0100

    opcode div divides the second top element of the stack by the top element of the stack

---
+++ div.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * div - divides the second top element of the stack by the top element of the
+ * stack
+ * @stack: pointer to the tp of stack
+ * @line_number: number of line
+ *
+ * Return: void
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	int divide;
+
+	if (!stack || !(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if (stack == 0 || (*stack) == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+
+	divide = temp->next->n / temp->n;
+	temp->next->n = divide;
+
+	*stack = temp->next;
+
+	free(temp);
+}
