7990a4731339569932b2c01485a171692b941d0a
CaroChoch <5692@holbertonstudents.com>
2022-12-14 06:12:33 -0800

    typo

--- mul.c
+++ mul.c
@@ -15,7 +15,7 @@ void mul(stack_t **stack, unsigned int line_number)
 
 	if (!stack || !(*stack) || !(*stack)->next)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
